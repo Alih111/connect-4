806aba474a88e628511fb1000058215fa2b74827
Ali Hassan <121449949+Alih111@users.noreply.github.com>
2024-03-20 00:35:37 +0200

    utility problem tree graph unfinished heuristic not done

--- connect 4.py
+++ connect 4.py
@@ -6,6 +6,9 @@ import pygame
 import sys
 import math
 import copy
+import networkx as nx
+import matplotlib.pyplot as plt
+import graphviz
 
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
@@ -14,26 +17,30 @@ YELLOW = (255, 255, 0)
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
-DEPTH=3
+DEPTH = 3
 PLAYER = 0
 AI = 1
-
+tree=[[]for i in range(DEPTH+1)]
 EMPTY = 0
 PLAYER_PIECE = 1
 AI_PIECE = 2
 
 WINDOW_LENGTH = 4
+
+
 def stringTo2d(string):
-    array_2d=[[]for i in range(ROW_COUNT)]
-    j=0
-    for i in range(COLUMN_COUNT*ROW_COUNT):
+    array_2d = [[] for i in range(ROW_COUNT)]
+    j = 0
+    for i in range(COLUMN_COUNT * ROW_COUNT):
         if (i % 7 == 0 and i != 0):
             j += 1
         array_2d[j].append(int(string[i]))
 
     return array_2d
 
+
 zeros_string = "0" * 42
+
 
 def create_board():
     board = np.zeros((ROW_COUNT, COLUMN_COUNT))
@@ -57,8 +64,6 @@ def get_next_open_row(board, col):
 
 def print_board(board):
     print(np.flip(board, 0))
-
-
 
 
 def winning_move(board, piece):
@@ -91,122 +96,130 @@ def winning_move(board, piece):
                 return True
 
 
-
-
 def draw_board(board):
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
             pygame.draw.rect(screen, BLUE, (c * SQUARESIZE, r * SQUARESIZE + SQUARESIZE, SQUARESIZE, SQUARESIZE))
             pygame.draw.circle(screen, BLACK, (
-            int(c * SQUARESIZE + SQUARESIZE / 2), int(r * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+                int(c * SQUARESIZE + SQUARESIZE / 2), int(r * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)), RADIUS)
 
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
             if board[r][c] == PLAYER_PIECE:
                 pygame.draw.circle(screen, RED, (
-                int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+                    int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
             elif board[r][c] == AI_PIECE:
                 pygame.draw.circle(screen, YELLOW, (
-                int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+                    int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
     pygame.display.update()
 
-def isFull(board): 
-    if 0 not in board[ROW_COUNT-1]:
+
+def isFull(board):
+    if 0 not in board[ROW_COUNT - 1]:
         return True
     return False
 
-def CalculateUtilityPiece(board,piece):
+
+def CalculateUtilityPiece(board, piece):
     score = 0
     # Check horizontal locations for win
-    for r in range(ROW_COUNT):      
+    for r in range(ROW_COUNT):
         for c in range(COLUMN_COUNT - 6):
-            length = 0         
-            while(c + length < 7 and board[r][c + length] == piece):
+            length = 0
+            while (c + length < 7 and board[r][c + length] == piece):
                 length += 1
             c += length
             if length > 3:
-                    score += length - 3
+                score += length - 3
 
     # Check vertical locations for win
-    for r in range(ROW_COUNT - 5):      
+    for r in range(ROW_COUNT - 5):
         for c in range(COLUMN_COUNT):
-            length = 0         
-            while(r + length < 6 and board[r + length][c] == piece):
+            length = 0
+            while (r + length < 6 and board[r + length][c] == piece):
                 length += 1
             r += length
             if length > 3:
-                    score += length - 3
+                score += length - 3
 
     # Check positively sloped diaganols
     pos_indcies = []
-    for r in range(ROW_COUNT - 3):      
+    for r in range(ROW_COUNT - 3):
         for c in range(COLUMN_COUNT - 3):
-            if r-c in pos_indcies:
+            if r - c in pos_indcies:
                 continue
             else:
                 for length in range(6, 3, -1):
-                    
-                    if all(r-c not in pos_indcies and r + i < 6 and c + i < 7 and board[r + i][c + i] == piece for i in range(length)):
+
+                    if all(r - c not in pos_indcies and r + i < 6 and c + i < 7 and board[r + i][c + i] == piece for i
+                           in range(length)):
                         print(f'length = {length},r = {r} and c = {c}')
                         print(f'before = {score}')
                         score += length - 3
                         length = 3
-                        pos_indcies.append(r-c)
-    #check negatively sloped diagonals
+                        pos_indcies.append(r - c)
+    # check negatively sloped diagonals
     neg_indices = []
     print('negative slope:')
-    for r in range(ROW_COUNT - 1,2,-1):      
+    for r in range(ROW_COUNT - 1, 2, -1):
         for c in range(COLUMN_COUNT - 3):
-            if r+c in neg_indices:
+            if r + c in neg_indices:
                 continue
             else:
 
                 if c == 3:
                     print('hi')
-                for length in range(6, 3, -1): 
-                    if all(r+c not in neg_indices and r - i < 6 and c + i < 7 and board[r - i][c + i] == piece for i in range(length)):
+                for length in range(6, 3, -1):
+                    if all(r + c not in neg_indices and r - i < 6 and c + i < 7 and board[r - i][c + i] == piece for i
+                           in range(length)):
                         print(f'length = {length},r = {r} and c = {c}')
                         score += length - 3
                         length = 3
-                        neg_indices.append(r+c)
+                        neg_indices.append(r + c)
     return score
+
+
 def evaluation(board):
-    if board[0][2]==2:
+    if board[0][2] == 2:
         return 2
     return 1
 
-def generateChildren(board,piece):
+
+def generateChildren(board, piece):
     children = []
     for col in range(COLUMN_COUNT):
         tempBoard = copy.deepcopy(board)
-        if is_valid_location(tempBoard,col):
-            row = get_next_open_row(tempBoard,col)
-            children.append(drop_piece(tempBoard,row,col,piece))
+        if is_valid_location(tempBoard, col):
+            row = get_next_open_row(tempBoard, col)
+            children.append(drop_piece(tempBoard, row, col, piece))
     return children
+
+
 def expectiMinMax(alpha, beta, depth, board, player):
-    score=0
-    a=copy.deepcopy(board)
-    while(1):
+    score = 0
+    a = copy.deepcopy(board)
+    while (1):
         choices = ['A', 'B', 'C']
         probabilities = [0.2, 0.6, 0.2]  # Probabilities must sum up to 1
         chosen = random.choices(choices, weights=probabilities, k=1)[0]
-        if(chosen=='A'):
-            if(is_valid_location(a, 0)):
-               r= get_next_open_row(a, 0)
-               drop_piece(a, r, 0, AI_PIECE)
-               break
-        elif(chosen=='C'):
-            if (is_valid_location(a, COLUMN_COUNT-1)):
-                r = get_next_open_row(a, COLUMN_COUNT-1)
-                drop_piece(a, r, COLUMN_COUNT-1, AI_PIECE)
+        if (chosen == 'A'):
+            if (is_valid_location(a, 0)):
+                r = get_next_open_row(a, 0)
+                drop_piece(a, r, 0, AI_PIECE)
+                break
+        elif (chosen == 'C'):
+            if (is_valid_location(a, COLUMN_COUNT - 1)):
+                r = get_next_open_row(a, COLUMN_COUNT - 1)
+                drop_piece(a, r, COLUMN_COUNT - 1, AI_PIECE)
                 break
         else:
             score, a = minMaxAlphaBeta(alpha, beta, depth, board, player)
             break
-    return score,a
+    return score, a
 
 
 def minMax(alpha, beta, depth, board, player):
+
     if isFull(board):
         PLAYER_Score = CalculateUtilityPiece(board, PLAYER_PIECE)
         AIScore = CalculateUtilityPiece(board, AI_PIECE)
@@ -222,6 +235,7 @@ def minMax(alpha, beta, depth, board, player):
         children = generateChildren(board, AI_PIECE)
         for child in children:
             utility, _ = minMaxAlphaBeta(alpha, beta, depth - 1, child, PLAYER)
+            tree[DEPTH - depth + 1].append((utility, _))
             if utility > maxUtility:
                 maxUtility = utility
                 maxChild = child  # Update the best child board
@@ -235,13 +249,17 @@ def minMax(alpha, beta, depth, board, player):
         children = generateChildren(board, PLAYER_PIECE)
         for child in children:
             utility, _ = minMaxAlphaBeta(alpha, beta, depth - 1, child, AI)
+            tree[DEPTH - depth + 1].append((utility, _))
             if utility < minUtility:
                 minUtility = utility
                 minChild = child  # Update the best child board
             if minUtility < beta:
                 beta = minUtility
         return minUtility, minChild
+
+
 def minMaxAlphaBeta(alpha, beta, depth, board, player):
+
     if isFull(board):
         PLAYER_Score = CalculateUtilityPiece(board, PLAYER_PIECE)
         AIScore = CalculateUtilityPiece(board, AI_PIECE)
@@ -250,13 +268,15 @@ def minMaxAlphaBeta(alpha, beta, depth, board, player):
 
     if depth == 0:
         return evaluation(board), board
-
     if player == 1:  # maximizing player
+
         maxUtility = -float('inf')
         maxChild = None  # Keep track of the best child board
         children = generateChildren(board, AI_PIECE)
+        i=0
         for child in children:
             utility, _ = minMaxAlphaBeta(alpha, beta, depth - 1, child, PLAYER)
+            tree[DEPTH - depth + 1].append((utility, ))
             if utility > maxUtility:
                 maxUtility = utility
                 maxChild = child  # Update the best child board
@@ -272,6 +292,7 @@ def minMaxAlphaBeta(alpha, beta, depth, board, player):
         children = generateChildren(board, PLAYER_PIECE)
         for child in children:
             utility, _ = minMaxAlphaBeta(alpha, beta, depth - 1, child, AI)
+            tree[DEPTH - depth + 1].append((utility, _))
             if utility < minUtility:
                 minUtility = utility
                 minChild = child  # Update the best child board
@@ -279,13 +300,28 @@ def minMaxAlphaBeta(alpha, beta, depth, board, player):
                 beta = minUtility
             if alpha >= beta:
                 break
-        return minUtility, minChild
 
+        return minUtility, minChild
+def get_total_elements(list_of_lists):
+  total_elements = 0
+  for inner_list in list_of_lists:
+    total_elements += len(inner_list)
+  return total_elements
+def drawTREE():
+    G = nx.BiGraph()
+    pos = {}
+    j = 0
+    wid = 0
+    for i in range(get_total_elements(tree)):
+        wid += 1
+        pos[i] = (10 * j, 15 * wid)
+        if (i % 7 == 0):
+            j += 2
+            wid = 0
 
 
 
 pygame.init()
-
 
 width = COLUMN_COUNT * 100
 height = (ROW_COUNT + 1) * 100
@@ -302,53 +338,50 @@ pygame.display.update()
 
 myfont = pygame.font.SysFont("monospace", 75)
 
-
-
-
 button_font = pygame.font.SysFont("Arial", 30)
-func=None
+func = None
 while True:
-        screen.fill((0, 0, 0))
-        flag=False
-        # Draw menu buttons
-        start_button = pygame.Rect(250, 200, 200, 50)
-        options_button = pygame.Rect(250, 280, 200, 50)
-        exit_button = pygame.Rect(250, 360, 200, 50)
+    screen.fill((0, 0, 0))
+    flag = False
+    # Draw menu buttons
+    start_button = pygame.Rect(250, 200, 200, 50)
+    options_button = pygame.Rect(250, 280, 200, 50)
+    exit_button = pygame.Rect(250, 360, 200, 50)
 
-        pygame.draw.rect(screen, (255, 0, 0), start_button)
-        pygame.draw.rect(screen, (0, 255, 0), options_button)
-        pygame.draw.rect(screen, (0, 0, 255), exit_button)
+    pygame.draw.rect(screen, (255, 0, 0), start_button)
+    pygame.draw.rect(screen, (0, 255, 0), options_button)
+    pygame.draw.rect(screen, (0, 0, 255), exit_button)
 
-        start_text = button_font.render("alpha beta", True, (0, 0, 0))
-        options_text = button_font.render("no alpha beta", True, (0, 0, 0))
-        exit_text = button_font.render("expectiminimax", True, (0, 0, 0))
+    start_text = button_font.render("alpha beta", True, (0, 0, 0))
+    options_text = button_font.render("no alpha beta", True, (0, 0, 0))
+    exit_text = button_font.render("expectiminimax", True, (0, 0, 0))
 
-        screen.blit(start_text, (270, 210))
-        screen.blit(options_text, (260, 290))
-        screen.blit(exit_text, (250, 370))
+    screen.blit(start_text, (270, 210))
+    screen.blit(options_text, (260, 290))
+    screen.blit(exit_text, (250, 370))
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = event.pos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = event.pos
 
-                if start_button.collidepoint(mouse_pos):
-                    flag=True
-                    func=minMaxAlphaBeta
-                    break
-                elif options_button.collidepoint(mouse_pos):
-                    flag = True
-                    func=minMax
-                    break
-                elif exit_button.collidepoint(mouse_pos):
-                    flag = True
-                    func=expectiMinMax
-                    break
-        if flag :
-            break
-        pygame.display.update()
+            if start_button.collidepoint(mouse_pos):
+                flag = True
+                func = minMaxAlphaBeta
+                break
+            elif options_button.collidepoint(mouse_pos):
+                flag = True
+                func = minMax
+                break
+            elif exit_button.collidepoint(mouse_pos):
+                flag = True
+                func = expectiMinMax
+                break
+    if flag:
+        break
+    pygame.display.update()
 
 board = create_board()
 game_over = False
@@ -397,17 +430,19 @@ while not game_over:
             # # Ask for Player 2 Input
         if turn == AI and not game_over:
             time.sleep(1)
-            sc,a=func(float('-inf'),float('inf'),DEPTH,board,AI)
-            board=a
+
+            sc, a = func(float('-inf'), float('inf'), DEPTH, board, AI)
+            tree[0].append((sc,a))
+            #draw tree
+            print(f"ai score ={CalculateUtilityPiece(board,AI_PIECE)} human score = {CalculateUtilityPiece(board,PLAYER_PIECE)}")
+            tree = [[] for i in range(DEPTH + 1)]
+            board = a
             draw_board(board)
-            turn =PLAYER
-
-
-
+            turn = PLAYER
 
         if isFull(board) == True:
-                label = myfont.render("Tie!!", 2, BLUE)
-                screen.blit(label, (40, 10))
-                game_over = True
+            label = myfont.render("Tie!!", 2, BLUE)
+            screen.blit(label, (40, 10))
+            game_over = True
     if game_over:
         pygame.time.wait(3000)
